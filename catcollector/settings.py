2f6fa54027b5382717564106ceb235f8a2144905
ManliestBen <benjamintmanley@gmail.com>
2024-06-05 15:05:11 -0500

    update settings

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+# CSRF_TRUSTED_ORIGINS = ["testy-cats-f831f6b5525c.herokuapp.com"]
 
 
 # Application definition
